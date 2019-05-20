c96fd4b8bf3ab80da345ea27c07b90331cd15384
Pei Wang <wangpei10@baidu.com>
2019-05-20 12:25:36 -0700

    Add obfuscation support

--- lib/CodeGen/BackendUtil.cpp
+++ lib/CodeGen/BackendUtil.cpp
@@ -66,6 +66,28 @@
 using namespace clang;
 using namespace llvm;
 
+namespace llvm {
+
+Pass *createPrivate0Pass();
+Pass *createPrivate1Pass();
+Pass *createPrivate2Pass();
+Pass *createPrivate3Pass();
+Pass *createPrivate4Pass();
+Pass *createPrivate5Pass();
+Pass *createPrivate6Pass();
+
+void populateObfuscationPassManager(legacy::PassManager &PM) {
+  PM.add(createPrivate0Pass());
+  PM.add(createPrivate1Pass());
+  PM.add(createPrivate2Pass());
+  PM.add(createPrivate3Pass());
+  PM.add(createPrivate4Pass());
+  PM.add(createPrivate5Pass());
+  PM.add(createPrivate6Pass());
+}
+
+}
+
 namespace {
 
 // Default filename used for profile generation.
@@ -701,6 +723,7 @@ void EmitAssemblyHelper::CreatePasses(legacy::PassManager &MPM,
 
   PMBuilder.populateFunctionPassManager(FPM);
   PMBuilder.populateModulePassManager(MPM);
+  populateObfuscationPassManager(MPM);
 }
 
 static void setCommandLineOpts(const CodeGenOptions &CodeGenOpts) {
